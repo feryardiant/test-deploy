8c376b1b96e4469b16ad4b349f1c9d9bbc1c5c11
Fery Wardiyanto <ferywardiyanto@gmail.com>
2024-09-19 15:12:36 +0700

    chore: update dev configs
    see: https://tailwindcss.com/blog/tailwindcss-v4-alpha#try-out-the-alpha
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,11 @@
 import Alpine from 'alpinejs'
+import axios from 'axios'
 
-import './bootstrap'
+import '@fontsource-variable/inter'
 
 window.Alpine = Alpine
+window.axios = axios
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 
 Alpine.start()
