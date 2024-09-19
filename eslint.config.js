89d7db3e1361eba5080de461b9e7d3c149984fca
Fery Wardiyanto <ferywardiyanto@gmail.com>
2024-09-19 11:42:18 +0700

    chore: install additional node-based dev dependencies
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+"use strict";
+
+import globals from "globals";
+
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+];
